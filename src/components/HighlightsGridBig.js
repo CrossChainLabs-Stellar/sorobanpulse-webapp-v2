c2e4b6cb2415a14d50b7d1c60e42331b75cd32e7
arctic-ash <andreea.stefan@crosschainlabs.tech>
2024-09-13 15:39:29 +0300

    Correct text

--- src/components/HighlightsGridBig.js
+++ src/components/HighlightsGridBig.js
@@ -79,7 +79,7 @@ const HighlightsGridBig = () => {
                     }}
                     align="center"
                 >
-                    Stellar XML
+                    Stellar XLM
                 </Typography >
             </Grid>
 
